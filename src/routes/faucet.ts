0da623f014085b38a1c0127609a910da8ac6fd74
Vojtech Simetka <vojtech@simetka.cz>
2022-06-02 11:51:06 +0200

    feat: add logging to the faucet service (#64)
    * feat: add logging to the faucet service
    
    * chore: import Logger should stay as type import

--- src/routes/faucet.ts
+++ src/routes/faucet.ts
@@ -22,6 +22,7 @@ import type { BlockEmitter } from '../lib/block-emitter'
 import type { Logger } from 'winston'
 import type { Contract } from '@ethersproject/contracts'
 import type { FundingConfig } from '../lib/config'
+import { logger } from '../lib/logger'
 
 export type FaucetRoutesConfig = {
   wallet: Wallet
@@ -51,7 +52,9 @@ function transformAddress(address: string): string {
 }
 
 async function hasBalance(provider: Provider, address: string): Promise<boolean> {
+  logger.debug(`checking balance of ${address}`)
   const balance = await provider.getBalance(address)
+  logger.info(`balance of ${address} is ${balance}`)
 
   return balance.gt(0)
 }
@@ -78,6 +81,7 @@ async function getNextBlockHash(wallet: Wallet, blockEmitter: BlockEmitter, bloc
 
       return nextBlock.hash
     }
+    logger.debug(`getNextBlockHash failed to get blockhash for blockNumber ${blockNumber}`)
 
     throw err
   }
@@ -85,6 +89,7 @@ async function getNextBlockHash(wallet: Wallet, blockEmitter: BlockEmitter, bloc
 
 async function createOverlayTx(wallet: Wallet, blockEmitter: BlockEmitter, address: string): Promise<OverlayTx> {
   if (await hasBalance(wallet.provider, address)) {
+    logger.info(`address ${address} already has balance`)
     throw new HasTransactionsError()
   }
 
@@ -95,6 +100,7 @@ async function createOverlayTx(wallet: Wallet, blockEmitter: BlockEmitter, addre
     data: '0x' + address.padStart(64, '0').toLowerCase(),
     gasPrice,
   })
+  logger.info(`sending transaction to ${address}`)
   const { blockNumber, blockHash, transactionHash } = await tx.wait()
   const nextBlockNumber = blockNumber + 1
 
@@ -116,6 +122,8 @@ async function fundAddressWithToken(bzz: Contract, address: string, amount: bigi
   const gasPrice = await bzz.getGasPrice()
   const tx = await bzz.transfer(address, amount, { gasPrice })
 
+  logger.debug(`fundAddressWithToken address ${address} amount ${amount}`)
+
   return await tx.wait()
 }
 
@@ -126,6 +134,8 @@ async function fundAddressWithNative(wallet: Wallet, address: string, amount: bi
     value: amount,
     gasPrice,
   })
+
+  logger.debug(`fundAddressWithNative address ${address} amount ${amount}`)
 
   return await tx.wait()
 }
